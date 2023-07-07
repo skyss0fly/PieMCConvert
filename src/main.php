f9df4658d3c9873ee0a0b9a56588948cceed3dcc
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-07 10:00:58 +1000

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,12 @@
+<?php
+namespace skyss0fly/PieMCConvert;
+
+class Main {
+
+public function OnEnable {
+$this->getLogger()->info("Converting your files. might take a bit...");
+$this->getLogger()->warning("Please not we will not be able to convert any plugins you have into PieMC at this current time");
+ $ops = $this->file_get_contents("ops.yml");
+  
+}
+}
